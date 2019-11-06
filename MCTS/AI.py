cc96a76421f583e9e4c271f75c49c48bcc2ce2ca
Fung Chi Tsam <1155094071@link.cuhk.edu.hk>
2019-11-07 00:19:08 +0800

    fix board size

--- MCTS/AI.py
+++ MCTS/AI.py
@@ -14,6 +14,7 @@ class AI:
         self.round = 0
         self.board = None
         self.keyboard = Controller()
+        self.previous_cur = [8, 2]
 
     def process_raw_board(self, line):
         board = []
@@ -51,6 +52,8 @@ class AI:
         for ch in string:
             self.keyboard.press(ch)
             self.keyboard.release(ch)
+            time.sleep(0.1)
+
 
 
     def ask_board(self):
@@ -60,28 +63,74 @@ class AI:
 
     def ask_round(self):
         self.string2keyboard("r")
-        line = self.wait_console("b")
-        self.round = int(line)
+        line = self.wait_console("r")
+        return int(line)
+
+    def offset_choice(self, row, col):
+        new_round = self.ask_round()
+        diff = new_round - self.round
+        return (row - diff, col)
+
+    def ask_cursor(self):
+        self.string2keyboard("c")
+        line = self.wait_console("c")
+        pos = line.split(" ")
+        print(pos)
+        self.previous_cur[0] = int(pos[0])
+        self.previous_cur[1] = int(pos[1])
 
     def send_position(self, row, col):
-        string = str(row) + "," + str(col) + "."
-        self.string2keyboard(string)
+        #string = str(row) + "," + str(col) + "."
+        #self.string2keyboard(string)
+        self.coord2keyboard(row, col)
+
+    def coord2keyboard(self, row, col):
+        row_dist = row - self.previous_cur[0]
+        col_dist = col - self.previous_cur[1]
+        ## if row_dist is negative: move up, otherwise down or no move
+        ## if col_dist is negative: move left, otherwise right or no move
+
+        if row_dist < 0:
+            str = "w" * abs(row_dist)
+            self.string2keyboard(str)
+        elif row_dist > 0:
+            str = "s" * row_dist
+            self.string2keyboard(str)
+        if col_dist < 0:
+            str = "a" * abs(col_dist)
+            self.string2keyboard(str)
+        elif col_dist > 0:
+            str = "d" * col_dist
+            self.string2keyboard(str)
+
+
+        self.string2keyboard(" ")
+        self.previous_cur[0] = row
+        self.previous_cur[1] = col
+
 
 if __name__ == "__main__":
     ai = AI()
-    time.sleep(3)
+    time.sleep(5)
 
-
+    count = 0
 
     while True:
-        ai.ask_board()
-        gameboard = GameBoard(ai.board)
-        num_available_choices = len(gameboard.get_available_choices())
-        init_state = State(gameboard.board, 0, [], num_available_choices)
-        root_node = Node(state=init_state)
+        if count == 0:
+            ai.ask_board()
+            ai.round = ai.ask_round()
+            gameboard = GameBoard(ai.board)
+            num_available_choices = len(gameboard.get_available_choices())
+            init_state = State(gameboard.board, 0, [], num_available_choices)
+            root_node = Node(state=init_state)
+            current_node = root_node
 
-        current_node = monte_carlo_tree_search(root_node)
+        current_node = monte_carlo_tree_search(current_node)
         choice = current_node.state.get_choice()
         print("You have choosen : " + str(choice[0]) + " " + str(choice[1]))
+        choice = ai.offset_choice(choice[0], choice[1])
+        ai.ask_cursor()
+
         ai.send_position(choice[0], choice[1])
         time.sleep(0.1)
+        count = (count + 1) % 3
